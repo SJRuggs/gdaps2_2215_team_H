da287364cc093c666463be5d8bc1734d19941f4f
SJRuggs <sjruggs03@gmail.com>
2022-02-13 23:42:19 -0500

    tile formatting complete

--- LiveWire/Game1.cs
+++ LiveWire/Game1.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 using System.IO;
 
 namespace LiveWire
 {
     /// <summary>
-    /// TODO: Add Summary
+    /// Level File Format:
+    /// 
+    /// rows,cols,tileWidth,tileHeight
+    /// (grid of ints separated by commas)
+    /// 
+    /// Level File Legend:
+    /// 0 - empty space
+    /// 1 - physical tile
+    /// TODO: and more to come!
     /// </summary>
 
     // --- FINITE STATE MACHINE : GAMESTATE ---
@@ -17,32 +26,52 @@ namespace LiveWire
         PlayLevel,
     }
 
+    enum Level
+    {
+        TestLevel,
+        EndLevel
+    }
+
     public class Game1 : Game
     {
 
         // --- VARIABLE DECLARATIONS ---
+
+        // graphics handlers
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         private int screenWidth;
         private int screenHeight;
         private SpriteFont basicFont;
+        private Texture2D tileSpriteSheet;
 
+        // stream handlers
         private StreamReader reader;
         private StreamWriter writer;
 
+        // input handlers
         private KeyboardState kbState;
         private KeyboardState prevKbState;
         private MouseState mState;
         private MouseState prevMState;
 
+        // player handlers
         private Player player;
         private int playerWidth;
         private int playerHeight;
 
+        // FSM handlers
+        private GameState currentState;
+        private Level currentLevel;
+
+        // board handlers
+        private Tile[,] board;
+        private int rows;
+        private int cols;
+        private int tileWidth;
+        private int tileHeight;
 
 
-        GameState currentState;
-        Tile[,] board; // TODO: Implement reader / writer logic
 
         public Game1()
         {
@@ -58,11 +87,12 @@ namespace LiveWire
             screenHeight = 1080;
             _graphics.PreferredBackBufferWidth = screenWidth;
             _graphics.PreferredBackBufferHeight = screenHeight;
+            _graphics.IsFullScreen = true;
             _graphics.ApplyChanges();
 
             // objects and states
             currentState = GameState.MainMenu;
-
+            currentLevel = Level.TestLevel;
 
             base.Initialize();
         }
@@ -71,8 +101,7 @@ namespace LiveWire
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             basicFont = Content.Load<SpriteFont>("BaseText");
-
-            // TODO: use this.Content to load your game content here
+            tileSpriteSheet = Content.Load<Texture2D>("LiveWireTiles");
         }
 
         protected override void Update(GameTime gameTime)
@@ -80,6 +109,8 @@ namespace LiveWire
             // update mnk
             kbState = Keyboard.GetState();
             mState = Mouse.GetState();
+
+            if (kbState.IsKeyDown(Keys.Escape)) { this.Exit(); }
 
             switch (currentState)
             {
@@ -98,6 +129,7 @@ namespace LiveWire
                     if (mState.LeftButton == ButtonState.Pressed && prevMState.LeftButton == ButtonState.Released)
                     {
                         currentState = GameState.PlayLevel;
+                        NewLevel(currentLevel);
                     }
                     break;
 
@@ -108,6 +140,7 @@ namespace LiveWire
                     if (SingleKeyPress(Keys.Enter, kbState, prevKbState))
                     {
                         currentState = GameState.MainMenu;
+                        currentLevel = Level.TestLevel;
                     }
                     break;
             }
@@ -164,11 +197,16 @@ namespace LiveWire
                                 screenWidth / 2 - (int)basicFont.MeasureString("Press LMB To Advance").X / 2,
                                 screenHeight / 2),
                         Color.Black);
+
+                    DrawLevel(currentLevel);
                     break;
 
 
 
                 case GameState.PlayLevel:
+                    // display level
+                    DrawLevel(currentLevel);
+
                     // TEMPORARY display
                     _spriteBatch.DrawString(
                         basicFont,
@@ -176,7 +214,7 @@ namespace LiveWire
                         new Vector2(
                                 screenWidth / 2 - (int)basicFont.MeasureString("Play Level Template").X / 2,
                                 screenHeight / 2 - (int)basicFont.MeasureString("Play Level Template").Y),
-                        Color.Black);
+                        Color.White);
 
                     _spriteBatch.DrawString(
                         basicFont,
@@ -184,7 +222,7 @@ namespace LiveWire
                         new Vector2(
                                 screenWidth / 2 - (int)basicFont.MeasureString("Press enter To Advance").X / 2,
                                 screenHeight / 2),
-                        Color.Black);
+                        Color.White);
                     break;
             }
 
@@ -192,14 +230,138 @@ namespace LiveWire
             base.Draw(gameTime);
         }
 
-        // --- HELPER METHODS ---
 
-        // detects a single key press, returns a boolean value
+
+        // --- HELPER METHODS ---
         private bool SingleKeyPress(Keys key, KeyboardState kbState, KeyboardState prevKbState)
         {
             if (prevKbState != null)
                 return kbState.IsKeyDown(key) && !prevKbState.IsKeyDown(key);
             return true;
+        }
+
+        private void NewLevel(Level level)
+        {
+            try 
+            {
+                // read info
+                string[] line;
+                string newLine;
+                reader = new StreamReader("../../../Levels/" + currentLevel + ".txt");
+                line = reader.ReadLine().Split(',');
+                rows = int.Parse(line[0]);
+                cols = int.Parse(line[1]);
+                tileWidth = int.Parse(line[2]);
+                tileHeight = int.Parse(line[3]);
+                bool temporaryBool;
+
+                // create board
+                board = new Tile[rows, cols];
+                for (int r = 0; r < rows; r++)
+                {
+                    newLine = reader.ReadLine();
+                    for (int c = 0; c < cols; c++)
+                    {
+                        // TODO: must be changed when interactable objects are implemented
+                        temporaryBool = newLine[c] != '0';
+                        board[r, c] = new Tile(c * tileWidth, r * tileHeight, tileWidth, tileHeight, tileSpriteSheet, temporaryBool);
+                    }
+                }
+
+                // --- ANIM STATES ---
+
+                // corner tiles
+                board[rows - 1, cols - 1].AnimState[5] = board[rows - 2, cols - 2].AnimState[0];
+                board[rows - 1, cols - 1].AnimState[6] = true;
+                board[rows - 1, cols - 1].AnimState[7] = true;
+                board[rows - 1, cols - 1].AnimState[8] = true;
+                board[rows - 1, 0].AnimState[5] = true;
+                board[rows - 1, 0].AnimState[6] = board[rows - 2, 1].AnimState[0];
+                board[rows - 1, 0].AnimState[7] = true;
+                board[rows - 1, 0].AnimState[8] = true;
+                board[0, 0].AnimState[5] = true;
+                board[0, 0].AnimState[6] = true;
+                board[0, 0].AnimState[7] = board[1, cols - 2].AnimState[0];
+                board[0, 0].AnimState[8] = true;
+                board[0, cols - 1].AnimState[5] = true;
+                board[0, cols - 1].AnimState[6] = true;
+                board[0, cols - 1].AnimState[7] = true;
+                board[0, cols - 1].AnimState[8] = board[1, 1].AnimState[0];
+
+                // edge tiles
+                for (int r = 1; r < rows - 1; r++)
+                {
+                    board[r, 0].AnimState[2] = !board[r, 1].AnimState[0];
+                    board[r, 0].AnimState[5] = true;
+                    board[r, 0].AnimState[6] = board[r - 1, 1].AnimState[0] && board[r, 1].AnimState[0] || board[r, 0].AnimState[6];
+                    board[r, 0].AnimState[7] = board[r + 1, 1].AnimState[0] && board[r, 1].AnimState[0] || board[r, 0].AnimState[7];
+                    board[r, 0].AnimState[8] = true;
+                    board[r, cols - 1].AnimState[4] = !board[r, cols - 2].AnimState[0];
+                    board[r, cols - 1].AnimState[5] = board[r - 1, cols - 2].AnimState[0] && board[r, cols - 2].AnimState[0];
+                    board[r, cols - 1].AnimState[6] = true;
+                    board[r, cols - 1].AnimState[7] = true;
+                    board[r, cols - 1].AnimState[8] = board[r + 1, cols - 2].AnimState[0] && board[r, cols - 2].AnimState[0];
+
+                }
+                for (int c = 1; c < cols - 1; c++)
+                {
+                    board[0, c].AnimState[3] = !board[1, c].AnimState[0];
+                    board[0, c].AnimState[5] = true;
+                    board[0, c].AnimState[6] = true;
+                    board[0, c].AnimState[7] = board[1, c + 1].AnimState[0] && board[1, c].AnimState[0] || board[0, c].AnimState[7];
+                    board[0, c].AnimState[8] = board[1, c - 1].AnimState[0] && board[1, c].AnimState[0] || board[0, c].AnimState[8];
+                    board[rows - 1, c].AnimState[1] = !board[rows - 2, c].AnimState[0];
+                    board[rows - 1, c].AnimState[5] = board[rows - 2, c + 1].AnimState[0] && board[rows - 2, c].AnimState[0] || board[rows - 1, c].AnimState[5];
+                    board[rows - 1, c].AnimState[6] = board[rows - 2, c - 1].AnimState[0] && board[rows - 2, c].AnimState[0] || board[rows - 1, c].AnimState[6];
+                    board[rows - 1, c].AnimState[7] = true;
+                    board[rows - 1, c].AnimState[8] = true;
+                }
+
+                // inside tiles
+                for (int r = 1; r < rows - 1; r++)
+                {
+                    for (int c = 1; c < cols - 1; c++)
+                    {
+                        if (board[r, c].AnimState[0])
+                        {
+                            board[r, c].AnimState[1] = !board[r - 1, c].AnimState[0];
+                            board[r, c].AnimState[2] = !board[r, c + 1].AnimState[0]; 
+                            board[r, c].AnimState[3] = !board[r + 1, c].AnimState[0]; 
+                            board[r, c].AnimState[4] = !board[r, c - 1].AnimState[0];
+                            board[r, c].AnimState[5] = board[r - 1, c - 1].AnimState[0] && board[r, c - 1].AnimState[0] && board[r - 1, c].AnimState[0];
+                            board[r, c].AnimState[6] = board[r - 1, c + 1].AnimState[0] && board[r, c + 1].AnimState[0] && board[r - 1, c].AnimState[0];
+                            board[r, c].AnimState[7] = board[r + 1, c + 1].AnimState[0] && board[r, c + 1].AnimState[0] && board[r + 1, c].AnimState[0];
+                            board[r, c].AnimState[8] = board[r + 1, c - 1].AnimState[0] && board[r, c - 1].AnimState[0] && board[r + 1, c].AnimState[0];
+                        }
+                    }
+                }
+
+                // all tile bridges
+                foreach (Tile tile in board)
+                {
+                    tile.AnimState[9]  = tile.AnimState[5] && tile.AnimState[8];
+                    tile.AnimState[10] = tile.AnimState[6] && tile.AnimState[7];
+                    tile.AnimState[11] = tile.AnimState[5] && tile.AnimState[6];
+                    tile.AnimState[12] = tile.AnimState[7] && tile.AnimState[8];
+                    tile.AnimState[13] = tile.AnimState[9] && tile.AnimState[10] && tile.AnimState[11] && tile.AnimState[12];
+                }
+            }
+            catch (Exception e) { Console.WriteLine(e); }
+            if (reader != null) { reader.Close(); }
+
+            // edit level FSM
+            currentLevel = level;
+        }
+
+        private void DrawLevel(Level level)
+        {
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    board[r, c].Draw(_spriteBatch);
+                }
+            }
         }
     }
 }
--- LiveWire/Tile.cs
+++ LiveWire/Tile.cs
@@ -2,22 +2,35 @@
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace LiveWire
 {
     /// <summary>
     /// TODO: Add summary
-    /// </summary>
+    /// </summary>  
     class Tile
     {
         // --- VARIABLE DELCARATIONS ---
         private Rectangle position;
-        private bool isActive;
-        private bool[] isBorder;
-            // 0: north border
-            // 1: east border
-            // 2: south border
-            // 3: west border
+        private bool[] animState;
+            // 0: base anim
+            // 1: N border
+            // 2: E border
+            // 3: S border
+            // 4: W border
+            // 5: NW inside
+            // 6: NE inside
+            // 7: SE inside
+            // 8: SW inside
+            // 9: west inside bridge
+            // 10: east inside bridge
+            // 11: north inside bridge
+            // 12: south inside bridge
+            // 13: all bridges
+            // 14: empty tile
+
+        private Texture2D spriteSheet;
 
 
 
@@ -28,31 +41,46 @@ namespace LiveWire
             set { position = value; }
         }
 
-        public bool IsActive
+        public bool[] AnimState
         {
-            get { return isActive; }
-            set { isActive = value; }
-        }
-
-        public bool[] IsBorder
-        {
-            get { return IsBorder; }
-            set { isBorder = value; }
+            get { return animState; }
+            set { animState = value; }
         }
 
 
 
         // --- CONSTRUCTOR ---
-        public Tile(int x, int y, int w, int h, bool isActive, bool[] isBorder)
+        public Tile(int x, int y, int w, int h, Texture2D spriteSheet, bool isActive)
         {
             position = new Rectangle(x, y, w, h);
-            this.isActive = isActive;
-            this.isBorder = isBorder;
+            this.spriteSheet = spriteSheet;
+            animState = new bool[15];
+            animState[0] = isActive;
+            animState[14] = !isActive;
         }
 
 
 
         // --- METHODS ---
-
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            for (int i = 0; i < animState.Length; i++)
+            {
+                if (animState[i])
+                {
+                    spriteBatch.Draw(
+                spriteSheet,
+                new Vector2(
+                    position.X,
+                    position.Y),
+                new Rectangle(
+                    i * position.Width,
+                    0,
+                    40,
+                    40),
+                Color.White);
+                }
+            }
+        }
     }
 }
